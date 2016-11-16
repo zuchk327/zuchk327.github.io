67982d97ae9596437f13719dc37d4ab219a456a2
Karolina Zuchowski <zuchk327@newschool.edu>
2016-11-16 19:44:52 +0000

    adding index file

---
+++ compfive/js/main.js
@@ -0,0 +1,28 @@
+$("body").css("background-color", "blue");
+
+var clicks = 0;
+
+$("button").click(function() {
+    clicks = clicks +1;
+    $("body").css("background-color","yellow");
+    console.log("clicks");
+});
+
+
+ 
+for (var i = 0; i < 50; i++)  {
+    $("body").append("<div class='dot'></div>");
+}
+
+var counter = 0;
+
+$(".dot").each(function() {
+    counter++;
+    console.log("found a dot....");
+    $(this).html(counter);
+});
+
+$(".dot").click(function() {
+    $(this).css("background-color","lime");
+});
+
