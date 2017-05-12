fca4eeb3b7dcdcd6fc10a0ede6f7c12893e97866
Karolina Zuchowski <zuchk327@newschool.edu>
2017-05-12 03:10:27 -0400

    Adding index file

--- poem-2/assets/js/poem.js
+++ poem-2/assets/js/poem.js
@@ -1,3 +1,48 @@
+$(".title").hover(function(){
+    $(".box").show()
+    $("*").css("background-color", "black")
+    $(".title").css("color", "white")
+    $(".title").css("animation-name", "none")
+  });
+
+$(".box").mouseover(function(){
+    $(".coin_release").show()
+    $(".box:hover").show()
+  });
+
+$("#c").mouseover(function(){
+    $(".box2").show()
+    
+  });
+
+$("#o").mouseover(function(){
+    $(".box3").show()
+    
+  });
+
+$("#i").mouseover(function(){
+    $(".box4").show()
+    
+  });
+
+$(".box2").mouseover(function(){
+    $(".num_2").show()
+    
+  });
+
+$(".box3").mouseover(function(){
+    $(".honeymoon").show()
+    
+  });
+
+$(".box4").mouseover(function(){
+    $(".american").show()
+    
+  });
+
+
+
+
 
 var objects = ['assets/img/video-1.mp4' ,
                 'assets/img/video-2.mp4' ,
